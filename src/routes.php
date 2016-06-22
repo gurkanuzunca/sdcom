3c76407a81ff038bd33f25fbeac16d2ef36c4859
Gürkan Uzunca <selam@gurkanuzunca.com>
2016-06-22 18:32:02 +0300

    Test!

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,4 @@
 <?php
 
 
-Route::add('/', 'HomeController', 'indexAction', 'get');
-Route::add('home/:slug', 'HomeController', 'indexAction', 'get');
-Route::get('home/:slug', 'HomeController', 'indexAction');
-Route::post('home/:slug', 'HomeController', 'indexAction');
+Route::get('/', 'HomeController', 'indexAction');
